06bb47f5ec766aa5da91f4a1232f8f6010019e26
Nathakit Leemeetham <77628492+JackzazaTH@users.noreply.github.com>
2025-11-04 17:20:03 +0700

    Delete uboncomputer directory

--- uboncomputer/app/shipping-policy/page.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-import React from 'react';
-import ShippingPolicyPage from '../../components/ShippingPolicyPage';
-import { useAppContext } from '../../context/AppContext';
-
-export default function ShippingPolicy() {
-  const { onNavigate } = useAppContext();
-  return <ShippingPolicyPage onNavigate={onNavigate} />;
-}

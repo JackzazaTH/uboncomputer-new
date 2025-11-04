6b174f62053c659daf3de3992c3d5cb2932212d8
Nathakit Leemeetham <77628492+JackzazaTH@users.noreply.github.com>
2025-11-04 17:11:00 +0700

    Delete uboncomputer directory

--- uboncomputer/app/cart/page.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-import React from 'react';
-import CartPage from '../../components/CartPage';
-import { useAppContext } from '../../context/AppContext';
-
-export default function Cart() {
-  const { onNavigate } = useAppContext();
-  return <CartPage onNavigate={onNavigate} />;
-}

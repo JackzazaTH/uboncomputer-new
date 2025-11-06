f60fe37424f63c474ed644c5b713bc2be943f4e9
Nathakit Leemeetham <77628492+JackzazaTH@users.noreply.github.com>
2025-11-06 09:12:55 +0700

    Delete uboncomputer directory

--- uboncomputer/app/about/page.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-import React from 'react';
-import AboutPage from '../../components/AboutPage';
-import { useAppContext } from '../../context/AppContext';
-
-export default function About() {
-  const { onNavigate } = useAppContext();
-  return <AboutPage onNavigate={onNavigate} />;
-}
--- uboncomputer/app/corporate/page.tsx
+++
@@ -1,7 +0,0 @@
-'use client';
-import React from 'react';
-import CorporatePage from '../../components/CorporatePage';
-
-export default function Corporate() {
-  return <CorporatePage />;
-}
